2b079960ad218e8b6f284bae1407834397ab54d4
lucadallaire <luca.dallaire@gmail.com>
2023-02-08 12:18:27 -0500

    updated the code to save the tweets to a csv file

--- twitter_api.py
+++ twitter_api.py
@@ -1,18 +1,23 @@
 import tweepy
 import pandas as pd
-import requests
+import config
 
 
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAHsplgEAAAAAGsWXI4bZ8lwpn74DVofP%2FVF0fxQ%3D7w2IO2jVZ7xpugZpMfa403agyquBzGKQZwqgsb4DmRwvPMWwKt"
+bearer_token = config.bearer_token
 
 # authentication
 
 client = tweepy.Client(bearer_token)
 
 # search tweets
-query = "fashion -is:retweet"
+query = "#fashion -is:retweet"
 tweets = client.search_recent_tweets(query=query, max_results=100)
 
 
+columns = ['ID', 'Tweet']
+data = []
 for tweet in tweets.data:
-    print(tweet)
+    data.append([tweet.id, tweet.text])
+
+df = pd.DataFrame(data, columns=columns)
+df.to_csv('tweets.csv')
